5236f2d385157d335dac8a2f957e3758f40a77a0
Mehrdad <mehrdadmrs10@gmail.com>
2026-05-08 00:21:17 +0330

    a test added to main to check performance of recordLayout class

--- main.cpp
+++ main.cpp
@@ -3,5 +3,15 @@
 
 int main(){
 
+    record r ;
+    r.name = "Mehrdad" ;
+    r.id = 123 ;
+    r.grade = 16.3 ;
+    r.isActive = 1 ;
+    recordLayout rl ;
+    std::vector<std::byte> data ;
+    data = rl.serializeRecord(r) ;
+    record r2 = rl.deserialize(data) ;
+
     return 0 ;
 }
