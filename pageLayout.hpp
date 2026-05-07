c6882879dd9f92ae601617e9f100e20c623ccc08
Mehrdad <mehrdadmrs10@gmail.com>
2026-05-08 00:50:54 +0330

    initPage func added to pageLayout class

--- pageLayout.hpp
+++ pageLayout.hpp
@@ -1,12 +1,27 @@
+#include"recordLayout.hpp"
 
-
-class pageLayout{
+class pageLayout
+{
 
 public:
+    std::vector<std::byte> initPage()
+    {
+        
+        std::vector<std::byte> page(512, static_cast<std::byte>(0));
 
-    
+        int recordCount = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            uint8_t byte = (recordCount >> (i * 8)) & 0xFF;
+            page[i] = static_cast<std::byte>(byte);
+        }
+
+        return page;
+    }
+
+
+
 
 private:
-
 
 };
