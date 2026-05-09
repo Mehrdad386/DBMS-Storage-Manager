230fcb4b7e1aac57c8d3f5912d93c7244583d13d
Mehrdad <mehrdadmrs10@gmail.com>
2026-05-09 12:58:22 +0330

    attributes of buffer pool added

--- BufferPool.hpp
+++ BufferPool.hpp
@@ -1,11 +1,24 @@
 #ifndef BUFFERPOOL
 #define BUFFERPOOL
+#include<vector>
+#include<unordered_map>
+#include<unordered_set>
+#include<cstddef>
+#include<list>
+#include<string>
 
 class BufferPool{
 
 public:
+    BufferPool(int capacity =3) : capacity(capacity){}
+
+
 
 private:
+    int capacity ;
+    std::unordered_map<int, std::vector<std::byte>> pages ;
+    std::list<int> lru ;
+    std::unordered_set<int> dirty ;
 
 
 };
